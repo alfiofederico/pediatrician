5b2a60e6b2fb20698ccbfc0fe29d8c1ae642662f
Alfio Federico Di Pinto <fefferico81@gmail.com>
2023-02-27 22:12:20 +0900

    fe

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ $("#navbar a, .btn").on("click", function (e) {
 
     $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top - 100,
+        scrollTop: $(hash).offset() - 100,
       },
       800
     );
